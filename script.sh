cd8c9592466cec2d6e10553e3073cbd5ff451e4d
Daniel Ring <dbr787@gmail.com>
2022-11-11 18:39:38 +1100

    test

--- script.sh
+++ script.sh
@@ -33,4 +33,5 @@ inline_image 'artifact://artifacts/image.gif' 'Rainbows'
 
 echo "${BUILDKITE_BUILD_URL}"
 echo "https://api.buildkite.com/v2/organizations/${BUILDKITE_ORGANIZATION_SLUG}/pipelines/${BUILDKITE_PIPELINE_SLUG}/builds/${BUILDKITE_BUILD_NUMBER}"
-# curl "https://api.buildkite.com/v2/organizations/${BUILDKITE_ORGANIZATION_SLUG}/pipelines/${BUILDKITE_PIPELINE_SLUG}/builds/${BUILDKITE_BUILD_NUMBER}"
+
+curl "https://api.buildkite.com/v2/organizations/${BUILDKITE_ORGANIZATION_SLUG}/pipelines/${BUILDKITE_PIPELINE_SLUG}/builds/${BUILDKITE_BUILD_NUMBER}"
