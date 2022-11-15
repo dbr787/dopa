a8fa87be44ba90bb30a59ea62ba722ff194d5cdb
Daniel Ring <dbr787@gmail.com>
2022-11-15 14:59:46 +1100

    fix bootstrap script permissions

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,9 +4,17 @@ set -eo pipefail
 
 echo "Executing bootstrap script"
 
+echo "Creating pre-bootstrap hook"
+
 cat <<EOF >>/etc/buildkite-agent/hooks/pre-bootstrap
 #!/bin/sh
 set -eo pipefail
 # Created on $(date)
-echo "This is a pre-bootstrap hook"
+echo "pre-bootstrap hook executed successfully"
 EOF
+
+echo "Changing permissions on pre-bootstrap hook"
+
+sudo chmod +x /etc/buildkite-agent/hooks/pre-bootstrap
+
+echo "bootstrap script executed successfully"
