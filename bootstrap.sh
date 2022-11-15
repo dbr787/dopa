6b70b432c26b7108275be434c0cd19b6bf8eb4ec
Daniel Ring <dbr787@gmail.com>
2022-11-15 17:22:33 +1100

    try pre-checkout hook

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,17 +1,41 @@
-#!/bin/sh
+#!/bin/bash
 
-set -eo pipefail
+set -eox pipefail
+
+# HOOK_TYPE="pre-bootstrap"
+HOOK_TYPE="pre-checkout"
 
 echo "Executing bootstrap script"
 
-echo "Creating pre-bootstrap hook"
+echo "Creating ${HOOK_TYPE} hook"
 
-cat <<EOF >>/etc/buildkite-agent/hooks/pre-bootstrap
-#!/bin/sh
-set -eo pipefail
+cat <<EOF >>/etc/buildkite-agent/hooks/${HOOK_TYPE}
+#!/bin/bash
+set -euxo pipefail
 # Created on $(date)
-echo "pre-bootstrap hook executed successfully"
-buildkite-agent annotate "pre-bootstrap hook executed successfully 🚀"
+
+# for line in "$(grep "^BUILDKITE_REPO=" "${BUILDKITE_ENV_FILE}")"
+# do
+#   repo="$(echo "${line}" | cut -d= -f2)"
+#   if [ "${repo}" != "git@server:repo.git" ]
+#   then
+#     echo "Repository not allowed: ${repo}"
+#     exit 1
+#   fi
+# done
+
+# for line in "$(grep "^BUILDKITE_COMMAND=" "${BUILDKITE_ENV_FILE}")"
+# do
+#   command="$(echo "${line}" | cut -d= -f2)"
+#   if [ "${command}" != "some-script.sh" ]
+#   then
+#     echo "Command not allowed: ${command}"
+#     exit 1
+#   fi
+# done
+
+echo "${HOOK_TYPE} hook executed successfully"
+buildkite-agent annotate "${HOOK_TYPE} hook executed successfully 🚀"
 EOF
 
 echo "Changing permissions on pre-bootstrap hook"
