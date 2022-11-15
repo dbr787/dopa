02517a4adaf07d133202dd595f2bb8304a067526
Daniel Ring <dbr787@gmail.com>
2022-11-15 17:31:17 +1100

    try pre-checkout hook

--- bootstrap.sh
+++ bootstrap.sh
@@ -8,18 +8,15 @@ HOOK_TYPE="pre-checkout"
 echo "Executing bootstrap script"
 
 echo "Creating ${HOOK_TYPE} hook"
-
 cat <<EOF >>/etc/buildkite-agent/hooks/${HOOK_TYPE}
 #!/bin/bash
 set -euxo pipefail
 # Created on $(date)
-
 echo "${HOOK_TYPE} hook executed successfully"
 buildkite-agent annotate "${HOOK_TYPE} hook executed successfully 🚀"
 EOF
 
-echo "Changing permissions on pre-bootstrap hook"
-
+echo "Changing permissions on ${HOOK_TYPE} hook"
 sudo chmod +x /etc/buildkite-agent/hooks/pre-bootstrap
 
 echo "bootstrap script executed successfully"
