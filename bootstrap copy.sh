bdcc5a5f3c907b24a41f86de75cee492f8fe1f16
Daniel Ring <dbr787@gmail.com>
2022-11-15 17:30:32 +1100

    try pre-checkout hook

--- bootstrap copy.sh
+++ bootstrap copy.sh
@@ -14,6 +14,26 @@ cat <<EOF >>/etc/buildkite-agent/hooks/${HOOK_TYPE}
 set -euxo pipefail
 # Created on $(date)
 
+# for line in "$(grep "^BUILDKITE_REPO=" "${BUILDKITE_ENV_FILE}")"
+# do
+#   repo="$(echo "${line}" | cut -d= -f2)"
+#   if [ "${repo}" != "git@server:repo.git" ]
+#   then
+#     echo "Repository not allowed: ${repo}"
+#     exit 1
+#   fi
+# done
+
+# for line in "$(grep "^BUILDKITE_COMMAND=" "${BUILDKITE_ENV_FILE}")"
+# do
+#   command="$(echo "${line}" | cut -d= -f2)"
+#   if [ "${command}" != "some-script.sh" ]
+#   then
+#     echo "Command not allowed: ${command}"
+#     exit 1
+#   fi
+# done
+
 echo "${HOOK_TYPE} hook executed successfully"
 buildkite-agent annotate "${HOOK_TYPE} hook executed successfully 🚀"
 EOF
