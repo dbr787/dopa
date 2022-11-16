a45ca42cfd14830e2d575abbce7ca00ee99b9f77
Daniel Ring <dbr787@gmail.com>
2022-11-16 21:31:27 +1100

    up

--- .buildkite/custom_upload.sh
+++ .buildkite/custom_upload.sh
@@ -7,7 +7,7 @@ echo "+++ :hammer: Running Custom Pipeline Upload"
 echo "original pipeline.yml"
 docker run --rm -v "${PWD}":/workdir mikefarah/yq -C "." .buildkite/pipeline.yml
 echo "override priority"
-OVERRIDE_PRIORITY=-1
+OVERRIDE_PRIORITY="-1"
 docker run --rm -v "${PWD}":/workdir mikefarah/yq -C "(.steps[] | select(has(\"command\"))).priority = $OVERRIDE_PRIORITY" .buildkite/pipeline.yml
 echo "buildkite-agent pipeline upload dry-run"
 buildkite-agent pipeline upload --dry-run
@@ -17,12 +17,12 @@ echo "yq pipeline json to yaml"
 docker run --rm -v "${PWD}":/workdir mikefarah/yq -P "." pipeline_output.json > pipeline_output.yaml
 cat pipeline_output.yaml
 echo "override priority in new yaml"
-OVERRIDE_PRIORITY=-1
+OVERRIDE_PRIORITY="-1"
 docker run --rm -v "${PWD}":/workdir mikefarah/yq -C "(.steps[] | select(has(\"command\"))).priority = $OVERRIDE_PRIORITY" pipeline_output.yaml > pipeline_override.yaml
 cat pipeline_override.yaml
 echo "upload pipeline with overrides"
-buildkite-agent pipeline upload pipeline_override.yaml
-
+# buildkite-agent pipeline upload pipeline_override.yaml
+buildkite-agent pipeline upload
 #  | docker run --rm -v "${PWD}":/workdir mikefarah/yq -C "(.steps[] | select(has(\"command\"))).priority = $OVERRIDE_PRIORITY"
 # OVERRIDE_PRIORITY=-1
 # docker run --rm -v "${PWD}":/workdir mikefarah/yq -C "(.steps[] | select(has(\"command\"))).priority = $OVERRIDE_PRIORITY" pipeline_output.json
