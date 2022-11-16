5bc8f095f544b2a2bde76bc7e06cba9a27b1661f
Daniel Ring <dbr787@gmail.com>
2022-11-16 22:01:48 +1100

    more

--- .buildkite/custom_upload.sh
+++ .buildkite/custom_upload.sh
@@ -7,27 +7,43 @@ echo "+++ :hammer: Running Custom Pipeline Upload"
 
 echo "original pipeline.yml"
 cat .buildkite/pipeline.yml
-echo "yq pipeline.yml"
-docker run --rm -v "${PWD}":/workdir mikefarah/yq -C "." .buildkite/pipeline.yml
+
+echo "dry-run output to json file"
+buildkite-agent pipeline upload --dry-run > pipeline_output.json
+cat pipeline_output.json
+
+echo "yq json file to override output file"
+OVERRIDE_PRIORITY="-1"
+docker run --rm -v "${PWD}":/workdir mikefarah/yq -C "(.steps[] | select(has(\"command\"))).priority = \"$OVERRIDE_PRIORITY\"" pipeline_output.json > override_output.json
+cat override_output.json
+
+echo "upload override file"
+buildkite-agent pipeline upload pipeline_override.yaml
+
+
+
+
+# echo "yq pipeline.yml"
+# docker run --rm -v "${PWD}":/workdir mikefarah/yq -C "." .buildkite/pipeline.yml
 # echo "override priority"
 # OVERRIDE_PRIORITY="-1"
 # docker run --rm -v "${PWD}":/workdir mikefarah/yq -C "(.steps[] | select(has(\"command\"))).priority = \"$OVERRIDE_PRIORITY\"" .buildkite/pipeline.yml
 # echo "buildkite-agent pipeline upload dry-run"
 # buildkite-agent pipeline upload --dry-run
 # echo "dry-run output to file"
-buildkite-agent pipeline upload --dry-run > pipeline_output.json
-echo "yq dry-run json to yaml (file cat)"
-docker run --rm -v "${PWD}":/workdir mikefarah/yq -P "." pipeline_output.json > pipeline_output.yaml
-cat pipeline_output.yaml
+# buildkite-agent pipeline upload --dry-run > pipeline_output.json
+# echo "yq dry-run json to yaml (file cat)"
+# docker run --rm -v "${PWD}":/workdir mikefarah/yq -P "." pipeline_output.json > pipeline_output.yaml
+# cat pipeline_output.yaml
 
-echo "yq override file"
-OVERRIDE_PRIORITY="-1"
-docker run --rm -v "${PWD}":/workdir mikefarah/yq -C "(.steps[] | select(has(\"command\"))).priority = \"$OVERRIDE_PRIORITY\"" pipeline_output.yaml > pipeline_override.yaml
-cat pipeline_override.yaml
+# echo "yq override file"
+# OVERRIDE_PRIORITY="-1"
+# docker run --rm -v "${PWD}":/workdir mikefarah/yq -C "(.steps[] | select(has(\"command\"))).priority = \"$OVERRIDE_PRIORITY\"" pipeline_output.yaml > pipeline_override.yaml
+# cat pipeline_override.yaml
 # echo "upload pipeline with overrides"
 # buildkite-agent pipeline upload pipeline_override.yaml
-echo "upload pipeline json"
-buildkite-agent pipeline upload pipeline_override.yaml
+# echo "upload pipeline json"
+# buildkite-agent pipeline upload pipeline_override.yaml
 # buildkite-agent pipeline upload pipeline_output.json
 # buildkite-agent pipeline upload
 #  | docker run --rm -v "${PWD}":/workdir mikefarah/yq -C "(.steps[] | select(has(\"command\"))).priority = $OVERRIDE_PRIORITY"
@@ -50,3 +66,4 @@ buildkite-agent pipeline upload pipeline_override.yaml
 # yq -C '.steps[].priority = 1' pipeline2.yml
 # This seems to add/update every command step to include priority = 1
 
+# docker run --rm -v "${PWD}":/workdir mikefarah/yq -C "(.steps[] | select(has(\"command\"))).priority = \"$OVERRIDE_PRIORITY\"" ./yaml-examples/test.json
